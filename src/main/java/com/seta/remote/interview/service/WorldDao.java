75b6d80b5745d1ce5aea5416ae5835a5764af57f
Giang Le <giang.le@aeris.com>
2021-07-28 12:05:01 +0700

    Add questions

---
+++ src/main/java/com/seta/remote/interview/service/WorldDao.java
@@ -0,0 +1,9 @@
+package com.seta.remote.interview.service;
+
+/**
+ *
+ * @author Binnur Kurt (binnur.kurt@gmail.com)
+ */
+public interface WorldDao extends CountryDao, com.example.dao.CityDao {
+
+}
